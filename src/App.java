0ba95d2453e1674436572033ac01f88b6ed43475
Jordan Cardek <jordancardek@hotmail.com>
2022-10-03 12:29:18 -0300

    Primeiro Commit da atividade

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {  
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quanto você ganha por hora?");
+        double valor = scan.nextDouble();
+
+        System.out.println("Quantas horas você trabalha no mês?");
+        double horas = scan.nextDouble();
+
+        scan.close();
+
+        double salario = valor*horas;
+
+        System.out.println("O seu salário no mês é de: "+salario);
+    }
+}
